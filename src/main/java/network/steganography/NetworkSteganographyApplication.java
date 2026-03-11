3b046676ff7644b979075b03b73485640baca47e
sunny <sunnybharti3.142@gmail.com>
2026-03-11 21:47:03 +0530

    first

---
+++ src/main/java/network/steganography/NetworkSteganographyApplication.java
@@ -0,0 +1,13 @@
+package network.steganography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkSteganographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkSteganographyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/network/steganography/NetworkSteganographyApplicationTests.java
@@ -0,0 +1,13 @@
+package network.steganography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkSteganographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
